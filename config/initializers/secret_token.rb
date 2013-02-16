560c317f2aa3fd362d68f8ca4040b3fc5c23734a
Dan Gilbert <dgilbert@groupon.com>
2013-02-16 16:37:23 -0600

    Scaffolding for POC app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChirpPoc::Application.config.secret_token = '69c3012651a20370f45171bfc9bd65e8a631d8f4ec9f00306fe443a23579c10f26da7300cb8f87fc6a795e091cdfb7e1d63b863ca405729bfc3a9c9f02e9b5cf'
